58b9dad5803afbd1ca87471f09058648679bed18
thaovy1994 <letranthaovy1326@gmail.com>
2025-06-03 16:55:11 +0700

    Test case - Empty Registration

--- src/test/java/Pages/RegisterPage.java
+++ src/test/java/Pages/RegisterPage.java
@@ -43,4 +43,35 @@ public class RegisterPage {
 //        ));
 //        System.out.println("Popup Toastify 'Bạn đã đăng ký thành công' đã hiển thị.");
     }
+
+    public void registerWithEmptyFields(){
+        driver.findElement(By.xpath("//a[@href='/register']")).click();
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//span[@class='text-danger' and contains(text(), ' Name không được bỏ trống ')]")
+        ));
+        System.out.println("Name không được bỏ trống");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//span[@class='text-danger' and contains(text(), '  Email không được bỏ trống ')]")
+        ));
+        System.out.println("Email không được bỏ trống");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//span[@class='text-danger' and contains(text(), '  Password không được bỏ trống ')]")
+        ));
+        System.out.println("Password không được bỏ trống");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//span[@class='text-danger' and contains(text(), '  PasswordConfirm không được bỏ trống ')]")
+        ));
+        System.out.println("PasswordConfirm không được bỏ trống");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//span[@class='text-danger' and contains(text(), ' Phone không được bỏ trống ')]")
+        ));
+        System.out.println("Phone không được bỏ trống");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//span[@class='text-danger' and contains(text(), 'Birthday không được bỏ trống')]")
+        ));
+        System.out.println("Birthday không được bỏ trống");
+    }
 }
--- src/test/java/Scripts/UserManager.java
+++ src/test/java/Scripts/UserManager.java
@@ -4,15 +4,15 @@ import Pages.RegisterPage;
 import org.testng.annotations.Test;
 
 public class UserManager extends BaseTest{
-//    @Test
-//    public void verifyLogin(){
-//        LoginPage loginPage = new LoginPage(driver);
-//        loginPage.login("0706169114","Cba321#@!");
-//    }
-
     @Test
     public void verifyValidRegistration(){
         RegisterPage registerPage = new RegisterPage(driver);
-        registerPage.validRegistration("mina","vy2@gmail.com","abc123","abc123","0790512345");
+        registerPage.validRegistration("mina","mina@gmail.com","abc123","abc123","0790512345");
+    }
+
+    @Test
+    public void  verifyEmptyRegistration(){
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.registerWithEmptyFields();
     }
 }
